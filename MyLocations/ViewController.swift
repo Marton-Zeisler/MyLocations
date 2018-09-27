adfe5d564575cde11a405fc6fa98d078cb0f50e8
Marton Zeisler <marton.zeisler@gmail.com>
2018-09-27 21:24:47 +0100

    Project Setup

---
+++ MyLocations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyLocations
+//
+//  Created by Marton Zeisler on 2018. 09. 27..
+//  Copyright © 2018. marton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
